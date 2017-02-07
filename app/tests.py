10ac9e341a03abf5a40978f3d7bf46ce1e7cf196
Yutong Huang <huang_yutong@outlook.com>
2017-02-06 20:50:31 -0500

    create tests dir

--- app/tests.py
+++ app/tests.py
@@ -1,10 +1,3 @@
-import pytest
+from django.test import TestCase
 
-# Simple function and test to verify Travis functionality
-def func(a):
-    return a + 1
-
-def test_func():
-    assert(func(3) == 4)
-    with pytest.raises(TypeError):
-        func('string')
+# Create your tests here.
