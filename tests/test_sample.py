0131d2d600e328957cb0ab6f6787b1ea3c076885
Yutong Huang <huang_yutong@outlook.com>
2017-02-13 17:22:12 -0500

    add second test

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -5,3 +5,12 @@ This file is to verify pytest and Travis functionality.
 def test_one():
     """Perform a trivial assert."""
     assert True
+
+def func(num):
+    """Increment a number by one."""
+    return num+1
+
+def test_two():
+    """Test func"""
+    assert func(3) == 4
+    assert func(2) == 5
