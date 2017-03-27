a4dd82d720e2d139ad6c657c20194fd0f9586617
Yexin Wang <Yexin Wang>
2017-03-27 05:48:24 -0400

    Add camera selection model

--- app/models.py
+++ app/models.py
@@ -22,3 +22,48 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     """Updates the assocaited Profile object whenever a User is updated."""
     instance.profile.save()
+
+
+#a model to represent a group of camera urls that a user has downloaded
+class User_Camera_Selection(models.Model):
+    #define the name of each object in the model
+    def __str__(self):
+        return self.user_obj.user.username + "'s camera selection"
+
+    def __unicode__(self):
+        return self.user_obj.user.username + "'s camera selection"
+
+    # associate to one User table
+    user_obj = models.ForeignKey(Profile, blank=True, null=True)
+
+    # selection info
+    number_of_selection = models.IntegerField(default=0)
+    date_of_download = models.DateTimeField(auto_now_add=True)
+    generated_key_for_download = models.CharField(max_length=100, unique=True)
+
+#the details of each camera selected by the user
+class Camera_Detail(models.Model):
+    # define the name of each object in the model
+    def __str__(self):
+        return self.url
+
+    def __unicode__(self):
+        return self.url
+
+    # associate to one User Camera Selection table
+    user_selection_obj = models.ForeignKey(User_Camera_Selection, blank=True, null=True)
+
+    # camera information
+    name = models.CharField(max_length=500, blank=True, null=True)
+    camera_key = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    url = models.CharField(max_length=200)
+    id = models.CharField(max_length=200)
+    date_of_selection = models.DateTimeField(auto_now_add=True)
+    frequency_of_selection = models.IntegerField(default=0)
+    is_ip_camera = models.BooleanField()
+    source = models.CharField(max_length=200, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=200, blank=True)
