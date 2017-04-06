61f48526f739fbb1da3ff7bf98cc36347be48952
Joseph Sweeney <sweeney2@purdue.edu>
2017-04-06 19:30:51 -0400

    modify selection models

--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from simple_history.models import HistoricalRecords #need to do pip install django-simple-history
 
 
 class Profile(models.Model):
@@ -25,39 +24,34 @@ def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
-#a model to represent a group of camera urls that a user has downloaded
-class User_Camera_Selection(models.Model):
-    #define the name of each object in the model
+class CameraSelection(models.Model):
+    """represents a group of camera URLs"""
+
     def __str__(self):
-        return self.user_obj.user.username + "'s camera selection"
+        return self.name
 
-    def __unicode__(self):
-        return self.user_obj.user.username + "'s camera selection"
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=30)  # TODO: Add default that looks at date
+    active = models.BooleanField(default=True)
 
-    # associate to one User table
-    user_obj = models.OneToOneField(User, blank=True, null=True)
+class CameraDetail(models.Model):
+    """the details of each camera selected by the user"""
 
-    # selection info
-    number_of_selection = models.IntegerField(default=0)
-    date_of_download = models.DateTimeField(auto_now_add=True)
-    history = HistoricalRecords()
-
-#the details of each camera selected by the user
-class Camera_Detail(models.Model):
-    # define the name of each object in the model
     def __str__(self):
-        return self.url
+        return self.name
 
-    def __unicode__(self):
-        return self.url
-
-    # associate to one User Camera Selection table
-    user_selection_obj = models.ForeignKey(User_Camera_Selection, blank=True, null=True)
-
-    # camera information
-    name = models.CharField(max_length=500, blank=True, null=True)
+    selection = models.ForeignKey(CameraSelection)
+    name = models.CharField(max_length=30)  # TODO: come up with default case
     url = models.CharField(max_length=200)
-    id = models.CharField(max_length=200)
-    date_of_selection = models.DateTimeField(auto_now_add=True)
-    frequency_of_selection = models.IntegerField(default=0)
-    history = HistoricalRecords()
+    cam_id = models.CharField(max_length=200)
+
+class SelectionDownload(models.Model):
+    """represents a single download of a selection"""
+
+    def __str__(self):
+        return self.selection  # TODO: Also return date and/or user
+
+    selection = models.ForeignKey(CameraSelection)
+    user = models.ForeignKey(User)
+    date = models.DateTimeField(auto_now_add=True)
