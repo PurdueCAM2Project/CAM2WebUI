f8bf70e2b0ec2b310e577e217ae121adbb14b156
Yutong Huang <huang_yutong@outlook.com>
2017-04-26 20:49:10 -0400

    able to load camera postion

--- cam2webui/settings.py
+++ cam2webui/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'social_django.middleware.SocialAuthExceptionMiddleware',
-
 ]
 
 ROOT_URLCONF = 'cam2webui.urls'
