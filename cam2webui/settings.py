b3fd61cc4e18de73604680396c8aca6831e4d523
Joseph Sweeney <sweeney2@purdue.edu>
2017-05-01 19:14:48 -0400

    remove debug

--- cam2webui/settings.py
+++ cam2webui/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Allow all host headers
 # NOTE: The value '*' is required by Heroku.
