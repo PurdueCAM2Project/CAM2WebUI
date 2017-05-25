3245434868ae30ff639c5d74230dbf68860bdcab
Juncheng Tang <tang184@purdue.edu>
2017-05-25 14:38:05 -0400

    github login

--- app/models.py
+++ app/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from django.contrib.auth.models import User
-
 class RegisterUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.CharField(verbose_name='Department(Optional)', max_length=100, blank=True, null=True)
@@ -13,3 +11,4 @@ class RegisterUser(models.Model):
     country = models.CharField(verbose_name='Country(Optional)', max_length=50, blank=True, null=True)
     about= models.TextField(verbose_name='About(Optional)', max_length=500, blank=True, null=True)
     email_confirmed = models.BooleanField(default=False)
+
