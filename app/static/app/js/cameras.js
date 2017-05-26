3060838789c8ce1c5335c3cbdefff9f07b7c0c82
Yutong Huang <huang_yutong@outlook.com>
2017-05-26 14:30:57 -0400

    back to only fusiontable load, without selection

--- app/static/app/js/cameras.js
+++ app/static/app/js/cameras.js
@@ -28,17 +28,7 @@ function initialize() {
         templateId: 2
       }
     });
-    /*
-    var column = layer.setOptions({
-      query: {
-        select: "Nation",
-        from: "1XszW34wSZP2dW4tfBJxX_Tnvmvvqnumd31WMIlxg",
-        groupby: "Nation"
-      }
-    });
-    
-    console.log(column);
-    */
+
     if (isMobile) {
       var legend = document.getElementById('googft-legend');
       var legendOpenButton = document.getElementById('googft-legend-open');
@@ -56,33 +46,3 @@ function initialize() {
       }
     }
   }
-
-function sendReq() {
-    var testing = new XMLHttpRequest();
-    testing.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            console.log(JSON.parse(this.responseText));
-        }
-    };
-    testing.open('GET', 'https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=AIzaSyBAJ63zPG5FpAJV9KXBJ6Y1bLKkvzYmhAg', true);
-    testing.send();
-}
-
-function start() {
-  gapi.client.init({
-    'apiKey': 'AIzaSyBAJ63zPG5FpAJV9KXBJ6Y1bLKkvzYmhAg',
-    'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-    'clientId': "143429239389-psh4a8i75tfm2545h2j582ieqc8fndbu.apps.googleusercontent.com",
-    'scope': 'profile'
-  }).then(function() {
-    return gapi.client.people.people.get({
-      'resourceName': 'people/me',
-      'requestMask.includeField': 'person.names'
-    });
-  }).then(function(response) {
-    console.log(response.result);
-  }, function(reason) {
-    console.log('Error: ' + reason.result.error.message);
-  });
-};
