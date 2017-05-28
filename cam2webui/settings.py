c874ee09ed10d635b48082db15774964a4f30434
Yutong Huang <huang_yutong@outlook.com>
2017-05-28 09:37:57 -0400

    make debug parameter false

--- cam2webui/settings.py
+++ cam2webui/settings.py
@@ -25,7 +25,7 @@ BASE_DIR =  os.path.dirname(PROJECT_ROOT)
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.cam2project.net',
