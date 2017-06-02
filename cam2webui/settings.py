1b11e5eb91a037c08693511051589a6b68d7f2d8
Yutong Huang <huang_yutong@outlook.com>
2017-06-02 16:01:06 -0400

    change host email to purdue email

--- cam2webui/settings.py
+++ cam2webui/settings.py
@@ -200,11 +200,11 @@ EMAIL_HOST = os.environ['EMAIL_HOST']
 EMAIL_PORT = os.environ['EMAIL_PORT']
 EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Release settings
