1ad04275765e497ad6de9704db3199f019feec65
Yutong Huang <huang_yutong@outlook.com>
2017-06-09 15:17:32 -0400

    add model to admin page

--- app/admin.py
+++ app/admin.py
@@ -4,3 +4,10 @@ from .models import FAQs
 # Register your models here.
 
 admin.site.register(FAQs)
+admin.site.register(History)
+admin.site.register(Publication)
+admin.site.register(Team)
+admin.site.register(Leader)
+admin.site.register(CurrentMember)
+admin.site.register(OldMember)
+
