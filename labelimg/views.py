f035aaf9156c9c4149fecdefbc04435a3a570f4a
Juncheng Tang <tang184@purdue.edu>
2017-07-11 17:05:28 -0400

    ftplib

--- labelimg/views.py
+++ labelimg/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
+from ftplib import FTP
+import json
+
 
 # Create your views here.
 def index(request):
     return render(request, 'labelimg/label.html')
+
+
+def getimg(request):
+	ftp = FTP('128.46.75.58')     # connect to host, default port
+	ftp.login()
+	print('success')
+	ftp.cwd('WD1')
+	#ftp.retrlines('LIST')
+	ftplist = ftp.nlst()
+	ftp.cwd(ftplist[0])
+
+	sub1 = ftp.nlst()
+	ftp.cwd(sub1[0])
+
+	a = ftp.nlst()
+
+	out = []
+
+	for element in a:
+		element = 'ftp://128.46.75.58/WD1/' + ftplist[0] + '/' + sub1[0] + '/' + element
+		out.append(element)
+
+	j = json.dumps(out)
+	print(j)
+
+	return j
