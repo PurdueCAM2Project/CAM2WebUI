b3ff2cf6cee5d824b67ba37337e811fad3e3dfa0
Deeptanshu Malik <deeptanshumalik@gmail.com>
2017-07-25 21:40:25 -0400

    mapped country codes to country viewports from geocode API

--- app/management/commands/load_latlngJSON.py
+++ app/management/commands/load_latlngJSON.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 import urllib.request, urllib.parse, urllib.error
+import requests
 import ssl
 from bs4 import BeautifulSoup
-
+import json
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
@@ -20,11 +21,29 @@ class Command(BaseCommand):
 
         countries = {}
         for country in country_menu:
-            countries[country['value']] =  str(country.text)
+            if country['value']:
+                countries[country['value']] =  str(country.text)
 
         return countries
 
+    def geocode_data(self, countries):
+        GOOGLE_MAPS_API_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+
+        for country_code, country_name in countries.items():
+            print(country_code, country_name)
+            # make the request and get the response data
+            req = requests.get(GOOGLE_MAPS_API_URL, params= {
+                'address': country_name
+            })
+            res = req.json()
+
+            #Use the first result
+            result = res['results'][0]
+
+            countries[country_code] = result['geometry']['viewport']
+        return countries
 
 
     def handle(self, *args, **options):
         countries = self.get_countries_from_webpage()
+        countries = self.geocode_data(countries)
