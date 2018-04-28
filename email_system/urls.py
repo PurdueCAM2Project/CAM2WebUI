37f7a16c2c0e6eabf96d25d138de18b8a79f96de
mardi08 <mardi@purdue.edu>
2018-04-28 17:20:06 -0400

    updated url for join us page

--- email_system/urls.py
+++ email_system/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^admin/$', views.admin_send_email, name='admin_send_email'),
     url(r'^contact/$', views.contact, name='contact'),
     url(r'^contact/email_sent/$', views.email_sent, name='email_sent'),
+    url(r'^join_us/$', views.join, name='join_us'),
 ]
