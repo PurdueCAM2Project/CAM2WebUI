158dbaaade3420d2d58e22b7e1f608db70bbfa5e
Chris <cjovanov@purdue.edu>
2018-05-29 10:01:23 -0400

    fixed stuff

--- app/admin.py
+++ app/admin.py
@@ -123,14 +123,13 @@ class UserAdmin(admin.ModelAdmin):
     country.short_description = 'country'
 
 
-#def user_priv(request):
 	
 
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 
-admin.site.egister(FAQ)
+admin.site.register(FAQ)
 admin.site.register(History)
 admin.site.register(Publication)
 admin.site.register(Team)
