37398f0e2b493a2ac3075f7aa476c08c14f0fc37
jcao216 <35784684+jcao216@users.noreply.github.com>
2018-06-04 15:23:37 -0400

    First Commit
    Contact Us section in admin page now shows items in a table format.

--- email_system/admin.py
+++ email_system/admin.py
@@ -12,42 +12,23 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 import csv
 
+
+class ContactModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'emailaddress', 'subject','message', 'date')
+	
+    def username(self, obj):
+        return obj.name
+    
+    def emailaddress(self, obj):
+        return obj.name
+
+class JoinModelAdmin(admin.ModelAdmin):
+    list_display= ('',)
+
+
+
+
 # Register your models here.
-admin.site.register(ContactModel)
-admin.site.register(JoinModel)
-
-
-class ContactAdmin(admin.ModelAdmin):
-    list_display = (
-        'username',
-        'email',
-        'first_name',
-        'last_name',
-        'department',
-        'organization',
-        'title',
-        'country',
-        'staff_status',
-        'date_joined',
-    )
-    # inlines = [
-        # UserInline,
-               # ]
-    # actions = [email_users, delete_users,export_csv]
-
-    # callable that display info from RegisterUser
-def department(self,user):
-        return "{}".format(RegisterUser.objects.get(user=user).department)
-    department.short_description = 'department'
-
-def organization(self,user):
-        return "{}".format(RegisterUser.objects.get(user=user).organization)
-    organization.short_description = 'organization'
-
-def title(self,user):
-        return "{}".format(RegisterUser.objects.get(user=user).title)
-    title.short_description = 'title'
-
-    def country(self,user):
-        return "{}".format(RegisterUser.objects.get(user=user).country)
-    country.short_description = 'country'
+admin.site.register(ContactModel, ContactModelAdmin)
+admin.site.register(JoinModel, )
+#admin.site.register()
