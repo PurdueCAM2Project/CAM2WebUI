2562bde812b6cb0b707983337a1082607b0d9113
Sion <hu440@purdue.edu>
2018-07-05 10:35:02 -0400

    Add Sponsors page

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import FAQ, History, Publication, Team, Leader, Member, RegisterUser, Collab, Location
+from .models import FAQ, History, Publication, Team, Leader, Member, RegisterUser, Collab, Location, Sponsor
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin #Important, dont remove
 from django.contrib.auth.models import User
@@ -139,6 +139,7 @@ admin.site.register(Publication)
 admin.site.register(Team)
 admin.site.register(Leader)
 admin.site.register(Collab)
+admin.site.register(Sponsor)
 admin.site.register(Location)
 admin.site.register(Member, MemberAdmin)
 
