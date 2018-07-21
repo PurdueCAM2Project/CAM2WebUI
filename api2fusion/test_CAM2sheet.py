42c68d76fb8e00804564ea5221e41e7b38f3c056
nhendy <nhendy@purdue.edu>
2018-07-21 19:31:47 -0400

    added testing class

---
+++ api2fusion/test_CAM2sheet.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from camera import *
+
+
+class TestCAM2sheet(TestCase):
+    def test_open(self):
+        self.fail()
+
+    def test_create(self):
+        self.fail()
+
+    def test_update(self):
+        self.fail()
+
+    def test_delete(self):
+        self.fail()
+
+    def test_get_ids(self):
+        self.fail()
