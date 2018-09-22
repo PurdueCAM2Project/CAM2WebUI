6ef36d0775acb0f76b994a2ec608586eedfdd64d
nhendy <nhendy@purdue.edu>
2018-09-22 00:56:33 -0400

    removed commented code

--- app/static/app/js/publication.js
+++ app/static/app/js/publication.js
@@ -5,20 +5,6 @@ $('.card-deck-wrapper').on('click', function(event) {
 
 $("#search_criteria").on("keyup", function () {
     var criterion = $(this).val().toLowerCase();
-    // $(".card .card-body .card-title, .card .card-body .card-subtitle, .card .card-body .card-text").each(function () {
-    //     var string = $(this).text();
-    //
-    //     if(string.indexOf(criterion) != -1)
-    //     {
-    //         console.log("I'm hiding");
-    //         $(this).parent().parent().hide();
-    //     }
-    //     else {
-    //         console.log("I'm showing");
-    //         $(this).parent().parent().show();
-    //     }
-    // });
-
     $(".card .card-body").each(function () {
         var paperInfo = $(this).find(".card-title").text().toLowerCase();
         var conference = $(this).find(".card-subtitle").text().toLowerCase();
@@ -31,20 +17,12 @@ $("#search_criteria").on("keyup", function () {
 
         if(!existsInAuthors && !existsInConference && !existsInPaperInfo)
         {
-            console.log("I'm hiding");
             $(this).parent().parent().hide();
         }
         else{
-            console.log("I'm show");
              $(this).parent().parent().show();
         }
     })
-
-
-
-
-
-
 
 });
 
