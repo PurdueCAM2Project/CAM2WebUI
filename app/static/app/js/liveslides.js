619ce51fbc1d896634c3c89c363610e6433931c5
Anirudh Vegesana <anirudh.vegesana@gmail.com>
2018-11-12 16:35:44 -0500

    Attempt to get IE support
    This commit attempts to add support for Internet Explorer. I don't think this feature is working properly for IE. I think we should make a version of this feature specifically for IE to get this to work, but that would increase the amount of code we need to maintain.

--- app/static/app/js/liveslides.js
+++ app/static/app/js/liveslides.js
@@ -3,6 +3,8 @@
 
 // Note slideId and slideNum are different. slideNum is the database column. slideId is the JS location in the carousel.
 !function(slideId, slideNum, maxSlides) {
+	"use strict";
+	
 	// Build livebox element
 	var lb = document.createElement("div");
 	lb.id = "livebox";
@@ -37,28 +39,29 @@
 
 	// Build image JSON
 	var clf = [
-{dx:206, dy:426, sx:600, sy:800, src:"http://206.140.121.226/axis-cgi/mjpg/video.cgi", still:"http://206.140.121.226/jpg/1/image.jpg",         name:"Cleveland Skyline"},
+{dx:206, dy:426, sx:600, sy:800, src:"http://206.140.121.226/axis-cgi/mjpg/video.cgi", still:"http://images.opentopia.com/cams/4472/big.jpg",  name:"Cleveland Skyline"},
 {dx:296, dy:380, sx:600, sy:800, src:"http://128.210.129.12/mjpg/video.mjpg",          still:"http://128.210.129.12/jpg/1/image.jpg",          name:"Purdue Memorial Union"},
-{dx:320, dy:300, sx:600, sy:800, src:"http://97.64.145.7/axis-cgi/mjpg/video.cgi",     still:"http://97.64.145.7/jpg/1/image.jpg",             name:"National Naval Association Museum"},
+{dx:320, dy:300, sx:600, sy:800, src:"http://97.64.145.7/axis-cgi/mjpg/video.cgi",     still:"http://images.opentopia.com/cams/15904/big.jpg", name:"National Naval Association Museum"},
 {dx:320, dy:200, sx:450, sy:800, src:"http://199.20.14.240/axis-cgi/mjpg/video.cgi",   still:"http://images.opentopia.com/cams/12609/big.jpg", name:"Schaumburg Lake"},
-{dx:132, dy:120, sx:251, sy:447, src:"http://128.206.143.98/axis-cgi/mjpg/video.cgi",  still:"http://128.206.143.98/jpg/1/image.jpg",          name:"Mizzou School of Journalism"},
-{dx:320, dy:740, sx:450, sy:800, src:"http://69.167.204.51/axis-cgi/mjpg/video.cgi",   still:"http://69.167.204.51/jpg/1/image.jpg",           name:"West Virginia University"},
+{dx:132, dy:120, sx:251, sy:447, src:"http://128.206.143.98/axis-cgi/mjpg/video.cgi",  still:"http://images.opentopia.com/cams/11358/big.jpg", name:"Mizzou School of Journalism"},
+//{dx:320, dy:740, sx:450, sy:800, src:"http://69.167.204.51/axis-cgi/mjpg/video.cgi",   still:"http://69.167.204.51/jpg/1/image.jpg",           name:"West Virginia University"},
 	];
 
 	// Display random images on the screen and build captions (captions not yet working)
 	var lb = document.getElementById("livebox");
 	var cl = shuffle(clf).slice(0, maxSlides);
+	var captEl = slide.getElementsByClassName("carousel-caption")[0];
+	captEl.class += " container";
+	// No IE: String.raw`<div class="row"><div class="col"></div><div class="col"></div><div class="col"></div></div>`
+	captEl.innerHTML = "<div class=\"row\"><div class=\"col\"></div><div class=\"col\"></div><div class=\"col\"></div></div>";
+	captEl.style="text-align: center;";
 	var caption = "";
-	with (slide.getElementsByClassName("carousel-caption")[0]) {
-		this.class += " container";
-		innerHTML = String.raw`<div class="row"><div class="col"></div><div class="col"></div><div class="col"></div></div>`;
-		style="text-align: center;";
-		var captions = getElementsByClassName("row")[0].getElementsByTagName("div");
-	}
+	var captions = captEl.getElementsByClassName("row")[0].getElementsByTagName("div");
 	for (var i = 0; i < cl.length; i++) {
 		caption += ", " + cl[i].name;
 		captions[i].innerText = cl[i].name;
-		lb.innerHTML += String.raw`<div class=liveslide dx=${cl[i].dx} dy=${cl[i].dy} sx=${cl[i].sx} sy=${cl[i].sy} still=${cl[i].still} name="${cl[i].name}"><img src="${cl[i].src}"></div>`;
+		// No IE: String.raw`<div class=liveslide dx=${cl[i].dx} dy=${cl[i].dy} sx=${cl[i].sx} sy=${cl[i].sy} still=${cl[i].still} name="${cl[i].name}"><img src="${cl[i].src}"></div>`
+		lb.innerHTML += "<div class=liveslide dx="+cl[i].dx+" dy="+cl[i].dy+" sx="+cl[i].sx+" sy="+cl[i].sy+" still="+cl[i].still+" name=\""+cl[i].name+"\"><img src=\""+cl[i].src+"\"></div>";
 	}
 	document.getElementById("welcome"+slideNum).innerText = caption.substring(2);
 
@@ -67,13 +70,14 @@
 
 	// Make a resize function to resize the images in the live slide (for responsiveness)
 	function resize(ds) {
-		with (ds) if (clientWidth != 0) {
+		if (ds.clientWidth != 0) {
 			// Show a default image while the real image is loading. In the future, this could be changed to a cached version of the camera.
-			style.backgroundImage = /*"url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='"+clientWidth+"' height='"+clientHeight+"'><rect width='100%' height='100%' style='fill:gray; fill-opacity: 0.7'/><text x='50%' y='50%' style='fill:lightBlue;' alignment-baseline='middle' text-anchor='middle' font-size='16'><tspan x='50%' dy='.6em'>Loading</tspan><tspan x='50%' dy='1.2em'>"+getAttribute("name")+"</tspan></text></svg>\")"*/"url(\""+getAttribute("still")+"\")";
+			ds.style.backgroundImage = /*"url(\"data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='"+ds.clientWidth+"' height='"+ds.clientHeight+"'><rect width='100%' height='100%' style='fill:gray; fill-opacity: 0.7'/><text x='50%' y='50%' style='fill:lightBlue;' alignment-baseline='middle' text-anchor='middle' font-size='16'><tspan x='50%' dy='.6em'>Loading</tspan><tspan x='50%' dy='1.2em'>"+ds.getAttribute("name")+"</tspan></text></svg>\")"*/"url(\""+ds.getAttribute("still")+"\")";
+			console.log(ds.clientWidth + " "+ ds.clientHeight);
 			// Reposition image so it is cropped in the correct location
-			getElementsByTagName("img")[0].width = clientWidth * 2;
-			scrollLeft = getAttribute("dx")/getAttribute("sx")*(scrollWidth-clientWidth);
-			scrollTop = getAttribute("dy")/getAttribute("sy")*(scrollHeight-clientHeight);
+			ds.getElementsByTagName("img")[0].width = ds.clientWidth * 2;
+			ds.scrollLeft = ds.getAttribute("dx")/ds.getAttribute("sx")*(ds.scrollWidth-ds.clientWidth);
+			ds.scrollTop = ds.getAttribute("dy")/ds.getAttribute("sy")*(ds.scrollHeight-ds.clientHeight);
 		}
 	};
 
